e6236495a85345de21111cd292bc029e27e2ddd1
Sombhu2022 <sombhudas93@gmail.com>
2023-12-11 00:03:03 +0530

    home page add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './global.css';
+import Home from './pages/Home.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-       <button>hii click me</button>
-    </div>
+    <Router>
+      <button>hii click me</button>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+
+    </Router>
+    
+   
   );
 }
 
---
+++ src/pages/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+   <div className="nav_container">
+
+
+   </div>
+  );
+}
+
+export default Header;
