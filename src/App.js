d016a775528047226e19831a0317747fedd4afb7
Sombhu2022 <sombhudas93@gmail.com>
2023-12-12 23:33:10 +0530

    favicon and links add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './global.css';
 import Home from './pages/Home.jsx';
-
+import Header from './pages/Header.jsx';
+import 'swiper/css'
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 function App() {
   return (
     <Router>
-      
+      <Header/>
+      {/* <Swiper
+       spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+      >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+
+      </Swiper> */}
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
